91e08ecaa7ce999860a9ab4351d8dd8925fe14f7
CristoCobos <cristotristan2003@gmail.com>
2026-05-21 16:55:55 -0600

    Refactor max clique algorithm with improved functions

--- src/pyrameters/clique.py
+++ src/pyrameters/clique.py
@@ -2,51 +2,47 @@ import networkx as nx
 
 def greedy_color(G_sub):
     color_class = []
-    k = 0
-    for i in G_sub.nodes():
+    for v in G_sub.nodes():
         h = 0
-        while h < k and (set(G_sub.neighbors(i)) & color_class[h]):
-            h += 1
-        if h == k:
-            k += 1
+        while h < len(color_classes):
+            if not ( set(G_sub.neighbors(v)) & color_classes[h]):
+                break
+            h +=1
+            
+        if h == len(color_classes):
             color_class.append(set())
-        color_class[h] = color_class[h] | {i}
-    return k, color_class
+        color_class[h].add(v)
+    return len(color_classes), color_class
 
-def greedy_bound(G, X: set):
-    if not X:
-        return len(G.nodes())
-    clique_nodes = list(X)
-    candidates = set(G.neighbors(clique_nodes[0]))
-    for nodo in clique_nodes[1:]:
-        candidates &= set(G.neighbors(nodo))
+def greedy_bound(G, clique, candidates):
     sub_candidates = G.subgraph(candidates)
-    return len(X) + greedy_color(sub_candidates)[0]
+    num_colors = greedy_color(sub_candidates)[0]
+    return len(clique) + num_colors
 
 def maxclique_solver(G):
     opt_clique = set()
     opt_size = 0
 
-    def _cliquebacktracking(X):
+    def expand(clique, candidate):
         nonlocal opt_clique, opt_size
-        
-        if len(X) > opt_size:
-            opt_size = len(X)
-            opt_clique = X.copy()
-            
-        if not X:
-            candidates = set(G.nodes())
-        else:
-            ultimo_nodo = max(X)
-            candidates = set(G.neighbors(ultimo_nodo)) & {v for v in G.nodes() if v not in X and v > ultimo_nodo}
-            
-        M = greedy_bound(G, X)
-        
-        for nodo in candidates:
-            if M <= opt_size:
-                return
-            _cliquebacktracking(X | {nodo})
 
-    _cliquebacktracking(set())
+        #actualizar mejor solución
+        if len(clique) > opt_size:
+            opt_size = len(clique)
+            opt_clique = clique.copy()
+
+        #Cota superior
+        M = greedy_bound(G, clique, candidates)
+
+        #Pruning
+        if M <= opt_size:
+            return
+
+        while candidates:
+            v = candidates.pop()
+            new_clique = (clique | {v})
+            new_candidates = (candidates & set(G.neighbors(v)))
+            expand(new_clique, new_candidates)
+    expand(clique=set(), candidates = set(G.nodes()))
     return opt_clique, opt_size
-
+    
