528cde57b1eca8fdcd3f466f99dbff5f36a335a2
or476651-orlando <or476651@uaeh.edu.mx>
2026-05-22 08:50:49 -0600

    Implement tests for graph parameters in test_all.py
    Add tests for various graph structures using pyrameters.

---
+++ tests/test_all.py
@@ -0,0 +1,141 @@
+import networkx as nx
+from src.all import pyrameters
+
+#############################
+# AUXILIAR
+#############################
+
+def es_clique(G, C):
+    """
+    Verifica que C induce un clique en G.
+    """
+    C = list(C)
+
+    for i in range(len(C)):
+        for j in range(i + 1, len(C)):
+            if not G.has_edge(C[i], C[j]):
+                return False
+
+    return True
+
+#######################
+# TESTS
+#######################
+
+def test_K15():
+
+    G = nx.complete_graph(15)
+    clique, omega, g, chi = pyrameters(G)
+    assert es_clique(G, clique)
+    assert omega == 15
+    assert len(clique) == 15
+    assert g == 3
+    assert chi == 15
+
+def test_C7():
+
+    G = nx.cycle_graph(7)
+    clique, omega, g, chi = pyrameters(G)
+    assert es_clique(G, clique)
+    assert omega == 2
+    assert len(clique) == 2
+    assert g == 7
+    assert chi == 3
+
+def test_C20():
+
+    G = nx.cycle_graph(20)
+    clique, omega, g, chi = pyrameters(G)
+    assert es_clique(G, clique)
+    assert omega == 2
+    assert len(clique) == 2
+    assert g == 20
+    assert chi == 2
+
+def test_empty():
+
+    G = nx.empty_graph(10)
+    clique, omega, g, chi = pyrameters(G)
+    assert es_clique(G, clique)
+    assert omega == 1
+    assert len(clique) == 1
+    assert g == None
+    assert chi == 1
+
+def test_petersen():
+
+    G = nx.petersen_graph()
+    clique, omega, g, chi = pyrameters(G)
+    assert es_clique(G, clique)
+    assert omega == 2
+    assert len(clique) == 2
+    assert g == 5
+    assert chi == 3
+
+def test_complete_bipartite():
+
+    G = nx.complete_bipartite_graph(5,10)
+    clique, omega, g, chi = pyrameters(G)
+    assert es_clique(G, clique)
+    assert omega == 2
+    assert len(clique) == 2
+    assert g == 4
+    assert chi == 2
+
+def test_heawood():
+
+    G = nx.heawood_graph()
+    clique, omega, g, chi = pyrameters(G)
+    assert es_clique(G, clique)
+    assert omega == 2
+    assert len(clique) == 2
+    assert g == 6
+    assert chi == 2
+
+def test_tutte():
+
+    G = nx.tutte_graph()
+    clique, omega, g, chi = pyrameters(G)
+    assert es_clique(G, clique)
+    assert omega == 2
+    assert len(clique) == 2
+    assert g == 4
+    assert chi == 3
+
+def test_disconnected():
+
+    G = nx.disjoint_union(
+    nx.complete_graph(4),
+    nx.path_graph(10)
+    )
+    clique, omega, g, chi = pyrameters(G)
+    assert es_clique(G, clique)
+    assert omega == 4
+    assert len(clique) == 4
+    assert g == 3
+    assert chi == 4
+
+
+def test_wheel():
+
+    G = nx.wheel_graph(8)
+    clique, omega, g, chi = pyrameters(G)
+    assert es_clique(G, clique)
+    assert omega == 3
+    assert len(clique) == 3
+    assert g == 3
+    assert chi == 4
+
+
+def test_path():
+
+    G = nx.path_graph(20)
+    clique, omega, g, chi = pyrameters(G)
+    assert es_clique(G, clique)
+    assert omega == 2
+    assert len(clique) == 2
+    assert g == None
+    assert chi == 2
+
+
+
