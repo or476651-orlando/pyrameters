e4873f6626e11c1d144fcf60338b875cd241c915
or476651-orlando <or476651@uaeh.edu.mx>
2026-05-22 09:12:32 -0600

    Rename function to calcular_numero_cromatico

--- src/pyrameters/__init__.py
+++ src/pyrameters/__init__.py
@@ -1,4 +1,4 @@
 from .all import pyrameters
 from .clique import calcular_clique
-from .chromatic import numero_cromatico
+from .chromatic import calcular_numero_cromatico
 from .girth import cuello
