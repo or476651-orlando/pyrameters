d74d21f28098d2084cd25368556e32c413f74d0d
or476651-orlando <or476651@uaeh.edu.mx>
2026-05-21 21:25:59 -0600

    Implement girth calculation for a graph
    Added a function to calculate the girth of a graph using BFS.

--- src/pyrameters/all.py
+++ src/pyrameters/all.py
@@ -40,7 +40,7 @@ def greedy_bound(G: nx.Graph, clique_actual: list, candidatos_actuales: set, N: 
 
 
 #############################
-#CLAN MAXIMO
+#NÚMERO DE CLAN
 #############################
 def maxclique(G: nx.Graph):
     opt_clique = set()
@@ -78,9 +78,9 @@ def maxclique(G: nx.Graph):
     return opt_size
     
 
-            
-
-
+##########################            
+#NÚMERO CROMÁTICO
+##########################
 def calcular_numero_cromatico(G: nx.Graph) -> int:
     """
     Calcula el número cromático (χ(G)) de una gráfica simple usando 
@@ -138,3 +138,58 @@ def calcular_numero_cromatico(G: nx.Graph) -> int:
     resolver(0, 0)
     
     return best_chi
+
+
+#######################
+#CUELLO O GIRTH
+#######################
+def girth(graph: nx.Graph):
+
+    G = clean(graph)
+
+    shortest = inf
+
+    # BFS from every vertex
+    for start in G.nodes():
+
+        dist = {start: 0}
+        parent = {start: None}
+
+        queue = deque([start])
+
+        while queue:
+
+            u = queue.popleft()
+
+            # Pruning
+            if 2 * dist[u] + 1 >= shortest:
+                continue
+
+            for v in G.neighbors(u):
+
+                # New vertex
+                if v not in dist:
+
+                    dist[v] = dist[u] + 1
+                    parent[v] = u
+
+                    queue.append(v)
+
+                # Found cycle
+                elif parent[u] != v:
+
+                    cycle_length = (
+                        dist[u]
+                        + dist[v]
+                        + 1
+                    )
+
+                    shortest = min(
+                        shortest,
+                        cycle_length
+                    )
+
+    if shortest == inf:
+        return None
+
+    return shortest
